88d9e059b02fa4a6c23223c09546010089c2716e
Shawn Bird <absent.bird@gmail.com>
2021-05-07 09:39:08 -0700

    refactored row adding function

--- docs/script.js
+++ docs/script.js
@@ -8,7 +8,7 @@
   var srtfile = 'captions.srt';
   document.querySelector('#srtupload').addEventListener('change', handleSrtUpload, false);
   document.querySelector('#timingupload').addEventListener('change', handleCsvUpload, false);
-  document.querySelector('#add-row').addEventListener('click', addPositioningRow, false);
+  document.querySelector('#add-row').addEventListener('click', addBlankRow, false);
   document.querySelector('#convert').addEventListener('click', downloadVtt, false);
   document.querySelector('.delete-row').addEventListener('click', removerow, false);
   document.body.addEventListener( 'click', function ( event ) {
@@ -41,6 +41,10 @@
     Papa.parse(event.target.result, {
       step: function(row) {
         console.log("Row:", row.data);
+        if (len(row) != 3) {
+          continue;
+        }
+        addrow(row[0],row[1],row[2]);
       },
       complete: function() {
         console.log("Done processing CSV timings.");
@@ -52,22 +56,38 @@
   var c1blank = document.getElementById('start1');
   var c2blank = document.getElementById('stop1')
   var c3blank = document.getElementById('position1')
-  
-  function addPositioningRow(event) {
+
+  function addrow(start, stop, position) {
     row = document.querySelector('#timingtable').insertRow(-1);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
     rowcount++;
-    cell1.appendChild(c1blank.cloneNode(true));
-    cell1.id = "start"+rowcount;
-    cell2.appendChild(c2blank.cloneNode(true));
-    cell2.id = "stop"+rowcount;
-    cell3.appendChild(c3blank.cloneNode(true));
-    cell3.id = "position"+rowcount;
+    var f1 = c1blank.cloneNode(true);
+    f1.id = "start"+rowcount;
+    if (start != "") {
+      f1.value = start;
+    }
+    cell1.appendChild(f1);
+    var f2 = c2blank.cloneNode(true);
+    f2.id = "stop"+rowcount;
+    if (stop != "") {
+      f2.value = stop;
+    }
+    cell2.appendChild(f2);
+    var f3 = c3blank.cloneNode(true);
+    f3.id = "position"+rowcount;
+    if (position != "") {
+      f3.value = position;
+    }
+    cell3.appendChild(f3);
     cell4.innerHTML = '<button class="delete-row">❌</button>';
     row.append
+  }
+
+  function addBlankRow(event) {
+    addrow();
   }
 
   function getsrt() {
